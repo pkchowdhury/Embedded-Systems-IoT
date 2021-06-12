a1c058983efd8a69b107266a79e6c1e2707e3336
Pallab Chowdhury <chowdhurypall95@gmail.com>
2021-06-12 17:20:44 +0600

    Add files via upload

---
+++ harry_potter_song_arduino_buzzer/harry_potter_song_arduino_buzzer.ino
@@ -0,0 +1,159 @@
+int treble = 8;
+
+void setup(){
+  pinMode(treble, OUTPUT);
+}
+
+void loop(){
+  
+  tone(treble, 493.88, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 1500/3);
+  delay(1500/3);
+  tone(treble, 783.99, 500/3);
+  delay(500/3);
+  tone(treble, 739.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 2000/3);
+  delay(2000/3);
+  tone(treble, 987.77, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 880.00, 3000/3);
+  delay(3000/3);
+  
+  tone(treble, 739.99, 3000/3);
+  delay(3000/3);
+  
+  tone(treble, 659.25, 1500/3);
+  delay(1500/3);
+  tone(treble, 783.99, 500/3);
+  delay(500/3);
+  tone(treble, 739.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 622.25, 2000/3);
+  delay(2000/3);
+  tone(treble, 698.46, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 493.88, 5000/3);
+  delay(5000/3);
+  tone(treble, 493.88, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 1500/3);
+  delay(1500/3);
+  tone(treble, 783.99, 500/3);
+  delay(500/3);
+  tone(treble, 739.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 2000/3);
+  delay(2000/3);
+  tone(treble, 987.77, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1174.66, 2000/3);
+  delay(2000/3);
+  tone(treble, 1108.73, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1046.50, 2000/3);
+  delay(2000/3);
+  tone(treble, 830.61, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1046.50, 1500/3);
+  delay(1500/3);
+  tone(treble, 987.77, 500/3);
+  delay(500/3);
+  tone(treble, 932.33, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 466.16, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 5000/3);
+  delay(5000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 987.77, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 987.77, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1046.50, 2000/3);
+  delay(2000/3);
+  tone(treble, 987.77, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 932.33, 2000/3);
+  delay(2000/3);
+  tone(treble, 739.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 783.99, 1500/3);
+  delay(1500/3);
+  tone(treble, 987.77, 500/3);
+  delay(500/3);
+  tone(treble, 932.33, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 466.16, 2000/3);
+  delay(2000/3);
+  tone(treble, 493.88, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 987.77, 5000/3);
+  delay(5000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 987.77, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 987.77, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1174.66, 2000/3);
+  delay(2000/3);
+  tone(treble, 1108.73, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1046.50, 2000/3);
+  delay(2000/3);
+  tone(treble, 830.61, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 1046.50, 1500/3);
+  delay(1500/3);
+  tone(treble, 987.77, 500/3);
+  delay(500/3);
+  tone(treble, 932.33, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 466.16, 2000/3);
+  delay(2000/3);
+  tone(treble, 783.99, 1000/3);
+  delay(1000/3);
+  
+  tone(treble, 659.25, 5000/3);
+  delay(5000/3);
+  
+}
+ 
