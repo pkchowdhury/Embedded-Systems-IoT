c9005366999ce33de27d9880fc4c66f00bbfc124
Pallab Chowdhury <chowdhurypall95@gmail.com>
2021-06-12 17:17:34 +0600

    Add files via upload

---
+++ ESP32_SimpleTime/ESP32_SimpleTime.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+#include "time.h"
+
+const char* ssid       = "WORKSTATION101";
+const char* password   = "WS@101-04";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  delay(500);
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void loop()
+{
+  delay(1000);
+  printLocalTime();
+  digitalWrite(2, HIGH);
+  delay(500);
+  
+}
